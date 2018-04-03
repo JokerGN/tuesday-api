eec1aef2cdb2fbdbd6bd0bcb0160e043de80b42e
JokerGN <chote1234@hotmail.co.th>
2018-04-04 01:09:55 +0700

    Fix bug not return log on first time equipment data insert

--- app/api/Equipment.js
+++ app/api/Equipment.js
@@ -7,12 +7,10 @@ import EquipLogRepository from '../resource/equipment/equip_log.repository'
 const Equipment = new Router()
 
 Equipment.post('/log_request', async function (context, next) {
-  let equipmentId = ''
   let data = context.request.body
   await EquipmentRepository.findOrCreate({imei: data.imei}, {imei: data.imei})
-  .spread((user, created) => {
-      equipmentId = user.equipmentId
-      EquipLogRepository.create({
+  .spread( async (user, created) => {
+      await EquipLogRepository.create({
         equipmentId: user.equipmentId,
         lat: data.lat,
         lng: data.lng,
@@ -22,11 +20,11 @@ Equipment.post('/log_request', async function (context, next) {
         alert_flag: data.alert_flag,
         log_time: data.datetime
       })
-  })
-  context.body = await EquipLogRepository.findBy({equipmentId: equipmentId}, {
-    scope: 'equip_logWithimei',
-    limit: 1,
-    order: Sequelize.literal('equipLogId DESC')
+      context.body = await EquipLogRepository.findBy({'$equipment.imei$': data.imei}, {
+        scope: 'equip_logWithimei',
+        limit: 1,
+        order: Sequelize.literal('equipLogId DESC')
+      })
   })
 })
 
