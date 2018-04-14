3f6b835bf89b86b96bd076621013847711c04874
JokerGN <chote1234@hotmail.co.th>
2018-04-14 13:19:22 +0700

    Add method get to get equipment log data

--- app/api/Equipment.js
+++ app/api/Equipment.js
@@ -3,6 +3,7 @@ import Sequelize from 'sequelize'
 import 'dotenv/config'
 import EquipmentRepository from '../resource/equipment/equipment.repository'
 import EquipLogRepository from '../resource/equipment/equip_log.repository'
+import { createDecipher } from 'crypto';
 
 const Equipment = new Router()
 
@@ -19,6 +20,28 @@ Equipment.post('/log_request', async function (context, next) {
         z: data.z,
         alert_flag: data.alert_flag,
         log_time: data.datetime
+      })
+      context.body = await EquipLogRepository.findBy({'$equipment.imei$': data.imei}, {
+        scope: 'equip_logWithimei',
+        limit: 1,
+        order: Sequelize.literal('equipLogId DESC')
+      })
+  })
+})
+
+Equipment.get('/log_request', async function (context, next) {
+  let data = context.request.query
+  await EquipmentRepository.findOrCreate({imei: data.imei},{imei: data.imei})
+  .spread( async (user, created) => {
+      await EquipLogRepository.create({
+        equipmentId: user.equipmentId,
+        lat: data.lat,
+        lng: data.lng,
+        x: data.x,
+        y: data.y,
+        z: data.z,
+        alert_flag: data.alert_flag,
+        log_time: data.log_time
       })
       context.body = await EquipLogRepository.findBy({'$equipment.imei$': data.imei}, {
         scope: 'equip_logWithimei',
