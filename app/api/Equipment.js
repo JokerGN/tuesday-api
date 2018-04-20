7a9f593e67bc330d97ac64d7b91718c3c72909be
JokerGN <chote1234@hotmail.co.th>
2018-04-20 10:53:46 +0700

    Change value of lat and lng data

--- app/api/Equipment.js
+++ app/api/Equipment.js
@@ -3,31 +3,31 @@ import Sequelize from 'sequelize'
 import 'dotenv/config'
 import EquipmentRepository from '../resource/equipment/equipment.repository'
 import EquipLogRepository from '../resource/equipment/equip_log.repository'
-import { createDecipher } from 'crypto';
 
 const Equipment = new Router()
 
-Equipment.post('/log_request', async function (context, next) {
-  let data = context.request.body.sad
-  await EquipmentRepository.findOrCreate({imei: data.imei}, {imei: data.imei})
-  .spread( async (user, created) => {
-      await EquipLogRepository.create({
-        equipmentId: user.equipmentId,
-        lat: data.lat,
-        lng: data.lng,
-        x: data.x,
-        y: data.y,
-        z: data.z,
-        alert_flag: data.alert_flag,
-        log_time: data.datetime
-      })
-      context.body = await EquipLogRepository.findBy({'$equipment.imei$': data.imei}, {
-        scope: 'equip_logWithimei',
-        limit: 1,
-        order: Sequelize.literal('equipLogId DESC')
-      })
-  })
-})
+//close because not use
+// Equipment.post('/log_request', async function (context, next) {
+//   let data = context.request.body.sad
+//   await EquipmentRepository.findOrCreate({imei: data.imei}, {imei: data.imei})
+//   .spread( async (user, created) => {
+//       await EquipLogRepository.create({
+//         equipmentId: user.equipmentId,
+//         lat: data.lat,
+//         lng: data.lng,
+//         x: data.x,
+//         y: data.y,
+//         z: data.z,
+//         alert_flag: data.alert_flag,
+//         log_time: data.datetime
+//       })
+//       context.body = await EquipLogRepository.findBy({'$equipment.imei$': data.imei}, {
+//         scope: 'equip_logWithimei',
+//         limit: 1,
+//         order: Sequelize.literal('equipLogId DESC')
+//       })
+//   })
+// })
 
 Equipment.get('/log_request', async function (context, next) {
   let data = context.request.query
@@ -35,8 +35,8 @@ Equipment.get('/log_request', async function (context, next) {
   .spread( async (user, created) => {
       await EquipLogRepository.create({
         equipmentId: user.equipmentId,
-        lat: data.lat,
-        lng: data.lng,
+        lat: (data.lat/100),
+        lng: (data.lng/100),
         x: data.x,
         y: data.y,
         z: data.z,
