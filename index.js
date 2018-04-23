2554b59a6b5f1ffe21429412010ff914d53a0265
JokerGN <chote1234@hotmail.co.th>
2018-04-23 23:27:34 +0700

    Implement socket.io for get equipment location

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ import Koa from 'koa'
 import Router from 'koa-router'
 import bodyParser from 'koa-bodyparser'
 import cors from '@koa/cors'
+import http from 'http'
 import 'dotenv/config'
-import GoogleMap from '@google/maps'
+import IO from 'socket.io'
 import Gallery from './app/api/Gallery'
 import Tour from './app/api/Tour'
 import Equipment from './app/api/Equipment'
+import EquipmentRepository from './app/resource/equipment/equipment.repository'
+import EquipLogRepository from './app/resource/equipment/equip_log.repository'
+import Sequelize from 'sequelize'
 
 const app = new Koa()
 const router = new Router()
@@ -23,5 +27,30 @@ router.use('/equipment', Equipment.routes())
 app.use(router.routes())
 app.use(router.allowedMethods())
 
+const server = http.createServer(app.callback())
+const io = new IO(server)
+
+io.on('connection', function (client) {
+  async function getdata () {
+    let data = await EquipmentRepository.findAndCountAllBy({},{})
+    let equip_id = []
+    data.rows.forEach(equip => {
+      equip_id.push(equip.imei)
+    })
+    equip_id.forEach( async function (equipment) {
+      let location = await EquipLogRepository.findBy({'$equipment.imei$': equipment}, {
+        scope: 'equip_logWithimei',
+        limit: 1,
+        order: Sequelize.literal('equipLogId DESC')
+      })
+      io.emit('connected',  location)
+    })
+  }
+  setInterval(getdata, 10000)
+  client.on('disconnect', function () {
+    console.log('client disconnect')
+  })
+})
+
 console.log('App listen at PORT '+process.env.PORT)
-app.listen(process.env.PORT)
+server.listen(process.env.PORT)
