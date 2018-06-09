86884285b13a9d486736d86f91b81b845ddde4a2
Thanachote Khaosanit <your_email_address@example.com>
2018-06-10 02:16:39 +0700

    Fix bug

--- app/api/Equipment.js
+++ app/api/Equipment.js
@@ -23,7 +23,7 @@ Equipment.get('/log_request', async function (context, next) {
       let lastLogTime = MomentTZ(lastLog[0].log_time).tz('Asia/Bangkok')
       let inputLogTime = Moment(data.log_time)
       let timediff = Moment.duration(inputLogTime.diff(lastLogTime))
-      if (timediff.hours() >= 0 && timediff.minutes() >= 10 || data.alert_flag === true) {
+      if (timediff.hours() >= 0 && timediff.minutes() >= 10 || data.alert_flag == true) {
         await EquipLogRepository.create({
           equipmentId: equipment[0].equipmentId,
           lat: latPos,
